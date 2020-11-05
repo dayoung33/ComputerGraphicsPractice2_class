bed7925192be5cd658fd33e031507c3c19f19b74
Dayoung Lee <rkek3766@gmail.com>
2020-11-05 15:55:11 +0900

    tutorial 2drendering+input

---
+++ Lighting_2DRendering_input/Engine/modelclass.h
@@ -0,0 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _MODELCLASS_H_
+#define _MODELCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <d3dx10math.h>
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "textureclass.h"
+
+#include <fstream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ModelClass
+////////////////////////////////////////////////////////////////////////////////
+class ModelClass
+{
+private:
+	struct VertexType
+	{
+		D3DXVECTOR3 position;
+	    D3DXVECTOR2 texture;
+		D3DXVECTOR3 normal;
+	};
+
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+	struct FaceType
+	{
+		int vIndex1, vIndex2, vIndex3;
+		int tIndex1, tIndex2, tIndex3;
+		int nIndex1, nIndex2, nIndex3;
+	};
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass&);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device*, const wchar_t*, const wchar_t*);
+	void Shutdown();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView* GetTexture();
+
+	bool ReadFileCounts(const wchar_t *);
+	bool LoadModel(const wchar_t*);
+	void ReleaseModel();
+
+
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	bool LoadTexture(ID3D11Device*, const wchar_t*);
+	void ReleaseTexture();
+
+private:
+	ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
+	int m_vertexCount, m_indexCount, m_textureCount, m_normalCount, m_faceCount;
+	TextureClass* m_Texture;
+
+	ModelType* m_model;
+};
+
+#endif
